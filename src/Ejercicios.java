c157c3b9a46a7e788908c583a465b754ac783859
mfernandez1305 <merarif@unitec.edu>
2014-09-24 22:19:37 -0600

    primer commit

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -23,70 +23,96 @@ public class Ejercicios {
 	//Escribe la variable contenido al inicio de un archivo que tiene como nombre nombre_archivo
 	static void escribirInt(String nombre_archivo, int contenido)
 	{
-		contenido=1;//se le asigna el valor a la variable contenido
-		String str=""+contenido;//se inicializa la variable y se le asigna la variable contenido a la variable string 
-		try{
+		try{//se intenta 
 			PrintWriter escritor= new PrintWriter(nombre_archivo);//se crea el escritor que exscribira en el archivo 
-			escritor.write(str);//se escribe en el archivo lo que contiene la variable str 
+			escritor.write(Integer.toString(contenido));//se define lo que se va escribir en este caso es la variable contenido
 			escritor.flush();
-			}catch(Exception e){//se atrapa cualquier error
-				e.printStackTrace();//se escribe el erro encontrado 
-				
-				
+			}catch(Exception e){//atrapa cualquier error
+				e.printStackTrace();//imprime el error
 			}
-		return;
+		
 	}
 	
 	//Devuelve el primer entero contenido en el archivo con nombre nombre_archivo
 	static int leerEntero(String nombre_archivo)
 	{
-		return -1;
+	try//se intenta 
+		{
+			
+			Scanner s = new Scanner(new File(nombre_archivo));//se crea el escaner 
+			    
+		return s.nextInt();// devuelve lo ingresado en el escaner 
+		}catch (Exception e)//atrapa cualquier error
+		{
+			e.printStackTrace();//imprime el error
+		}
+		return 0 ;//devuelve cero 
 	}
+	//me guie con el ejercicio de mi compañero https://github.com/FabianTr13/Prog_t5/blob/master/src/Ejercicios.java
 	
 	//Devuelve la primera string contenida en el archivo con nombre nombre_archivo
 	static String leerString(String nombre_archivo)
 	{
+		try//se intenta 
+		{
+			
+		Scanner s = new Scanner(new File(nombre_archivo));// se crea el escaner 
+
+		return s.next();//devuelve lo que se ingresa en el escaner 
+		}catch (Exception e)//atrapa cualquier error 
+		{
+			e.printStackTrace();//imprime el error 
+		}
+		
 		return "";
 	}
 	
 	//Devuelve el primer double contenido en el archivo con nombre nombre_archivo
 	static double leerDouble(String nombre_archivo)
 	{
+		try//se intenta 
+		{
+			
+			Scanner s = new Scanner(new File(nombre_archivo));//se crea el scanner
+		   
+		return s.nextDouble();// devuelve lo ingresado en el escaner 
+		}catch (Exception e)// atrapa cualquier error 
+		{
+			e.printStackTrace();//imprime el error
+		}
+		
 		return -1;
 	}
 	
 	//Devuelve si el archivo con nombre nombre_archivo contiene la string buscada
 	static boolean existe(String nombre_archivo, String buscada)
 	{
-		String str="";//se declara la variable str y se inicializa
+		
 		try{
-			File archivo= new File ("nombre_archivo");
-			Scanner s= new Scanner (nombre_archivo);// se coloca el escaner que permitira ingresar datos 
+		
+			Scanner s= new Scanner (new File(nombre_archivo));// se coloca el escaner que permitira ingresar datos 
 			while(s.hasNext()){// se inicia el while leyendo los datos que estan en el archivo
-				
-				buscada=s.next();// permite ingresar la variable a buscar 
-				if(str.equals(buscada))//se evalua si string es igual que el contenido de la variable buscada 
-					str=buscada;// si esto sucede se igalan las variables 
 			
-			}	
-			
-		}catch//atrapa cualquier error
-		(Exception e){
-			e.printStackTrace();//imprime el error
+			if(s.next().equals(buscada))//se evalua si el escaner es igual que el contenido de la variable buscada, esta linea la copie de la correccion hecha a alex fabian en http://pastebin.com/ysRAkkdU
+		return true;//se devuelve true
+			}
+		}catch(Exception e){//atrapa cualquier error
+		e.printStackTrace();//imprime el error
 		}
-		return false;//devuelve falso
+		
+		return false;//devuelve falso 
 	}
 	
 	//Devuelve el numero menor de un archivo
 	//Dado, el archivo unicamente contiene enteros
 	static int getNumeroMenor(String nombre_archivo)
 	{
+		//este ejercicio esta corregido con la correcion de mi examen en http://pastebin.com/dtdtuk3S
 		int menor=10000;//se declara y se inicializa la variable menor 
-		String str=""+menor;// se declara y se inicializa la variable str
 		
 		try{
-		File archivo= new File ("nombre_archivo");
-		Scanner s= new Scanner (nombre_archivo);// se coloca el escaner que permitira ingresar datos 
+		
+		Scanner s= new Scanner (new File(nombre_archivo));// se coloca el escaner que permitira ingresar datos 
 		
 		while(s.hasNext()){// se inicia el while leyendo los datos que estan ene l archivo
 			int num=s.nextInt();//se declara la variable num 
@@ -100,7 +126,7 @@ public class Ejercicios {
 		e.printStackTrace();//imprime el error 
 	}
 		
-		return -1;
+		return menor;
 	}
 
 	
